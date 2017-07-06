501f29a18a08c53926ff3eec3beceb2a10ee567e
hariKK <hari.itsolution@gmail.com>
2017-07-06 17:04:31 +0530

    Commit updated file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent,LoginComponent],
+  imports:      [BrowserModule],
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component(
+{
+    selector:'login',
+    templateUrl:'./login.html',       
+}
+
+)
+export class LoginComponent
+{
+    //let isLogin: boolean =false;
+    constructor()
+    {
+        
+    }
+
+}
